daf6be8a824d50b7603fa53907577184330a1bd2
Tze-chiu Lei <tclei2009@gmail.com>
2022-03-05 19:26:17 -0700

    Add ng-bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
+import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
 import { splashVideoReducer } from './store/reducers/splash-video.reducer';
 
@@ -19,6 +20,7 @@ import { splashVideoReducer } from './store/reducers/splash-video.reducer';
       name: 'blackswanarchery',
       logOnly: environment.production,
     }),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
