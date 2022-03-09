3b3a82a2d17f9c4b39bcd2a1716c4d29797b6bac
Tze-chiu Lei <tclei2009@gmail.com>
2022-03-08 19:46:08 -0700

    Add NGRX router-store.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    StoreRouterConnectingModule.forRoot(),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
