28a6ec9b167e50543517fb9b86831de697adc97a
Tze-chiu Lei <tclei2009@gmail.com>
2022-03-13 17:19:16 -0700

    Add actions tests to AppComponent unit-tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TestBed } from '@angular/core/testing';
 import { LocationStrategy } from '@angular/common';
 import { MockLocationStrategy } from '@angular/common/testing';
-
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppState } from './models/app-state.model';
 import { AppComponent } from './app.component';
+import * as svActions from './store/actions/splash-video.actions';
 
 describe('AppComponent', () => {
-  let store: MockStore;
-
   const initialState: AppState = {
     router: {
       state: {
@@ -30,6 +28,7 @@ describe('AppComponent', () => {
       done: false,
     },
   };
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -42,6 +41,7 @@ describe('AppComponent', () => {
     }).compileComponents();
 
     store = TestBed.inject(MockStore);
+    store.setState(initialState);
   });
 
   it('should create the app', () => {
@@ -56,16 +56,57 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Black Swan Archery');
   });
 
-  it('should call finishSplashVideo method on Finish button click', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    const finishMethodSpy = spyOn(app, 'finishSplashVideo');
-    let finishButton = fixture.debugElement.nativeElement.querySelector(
-      '[data-testid="btnFinishSplashVideo"]',
-    );
+  describe('Finish splash-video', () => {
+    it('should call finishSplashVideo method on Finish button click', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      const finishMethodSpy = spyOn(app, 'finishSplashVideo');
+      let finishButton = fixture.debugElement.nativeElement.querySelector(
+        '[data-testid="btnFinishSplashVideo"]',
+      );
 
-    finishButton.click();
-    fixture.detectChanges();
-    expect(finishMethodSpy).toHaveBeenCalled();
+      finishButton.click();
+      store.refreshState();
+
+      expect(finishMethodSpy).toHaveBeenCalled();
+    });
+    it('should dispatch Finish action from finishSplashVideo method', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      const dispatchSpy = spyOn(store, 'dispatch');
+      const expectedAction = svActions.finish();
+
+      app.finishSplashVideo();
+      store.refreshState();
+
+      expect(dispatchSpy).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+
+  describe('Reset splash-video', () => {
+    it('should call resetSplashVideo method on Reset button click', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      const resetMethodSpy = spyOn(app, 'resetSplashVideo');
+      let resetButton = fixture.debugElement.nativeElement.querySelector(
+        '[data-testid="btnResetSplashVideo"]',
+      );
+
+      resetButton.click();
+      store.refreshState();
+
+      expect(resetMethodSpy).toHaveBeenCalled();
+    });
+    it('should dispatch Reset action from resetSplashVideo method', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      const dispatchSpy = spyOn(store, 'dispatch');
+      const expectedAction = svActions.reset();
+
+      app.resetSplashVideo();
+      store.refreshState();
+
+      expect(dispatchSpy).toHaveBeenCalledWith(expectedAction);
+    });
   });
 });
