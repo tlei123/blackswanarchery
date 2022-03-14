fb6007956a702359effbc3a312172c723e1e2d35
Tze-chiu Lei <tclei2009@gmail.com>
2022-03-13 19:32:10 -0700

    Use cypress testing-library in template/spec.

--- cypress/integration/app.cypress.spec.ts
+++ cypress/integration/app.cypress.spec.ts
@@ -1,8 +1,34 @@
 describe('App component', () => {
-  it('Visits the initial app page', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.contains('Black Swan Archery').should('be.visible');
-    cy.contains('Router State').should('be.visible');
-    cy.contains('Splash Video State').should('be.visible');
+  });
+
+  it('Visits the initial app page', () => {
+    cy.findByTestId('appHeader').should('be.visible');
+    cy.findByTestId('routerStateHeader').should('be.visible');
+    cy.findByTestId('splashVideoStateHeader').should('be.visible');
+  });
+
+  context('Router state', () => {
+    it('Shows url:/ for url', () => {
+      cy.findByTestId('routerStateUrl').should('have.text', '/');
+    });
+    it('Shows root.outlet:primary for root.outlet', () => {
+      cy.findByTestId('routerStateRootOutlet').should('have.text', 'primary');
+    });
+  });
+
+  context('Splash Video state', () => {
+    it('Shows done: false as initial state', () => {
+      cy.findByTestId('splashVideoStateDone').should('have.text', 'false');
+    });
+    it('Shows done: true on Finish button click', () => {
+      cy.findByTestId('btnFinishSplashVideo').click();
+      cy.findByTestId('splashVideoStateDone').should('have.text', 'true');
+    });
+    it('Shows done: false on Reset button click', () => {
+      cy.findByTestId('btnResetSplashVideo').click();
+      cy.findByTestId('splashVideoStateDone').should('have.text', 'false');
+    });
   });
 });
