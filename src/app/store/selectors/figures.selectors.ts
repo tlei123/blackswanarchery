c2d4818e491bdd8f1dcbaf9ccf4fab2669bea2cb
Tze-chiu Lei <tclei2009@gmail.com>
2022-03-20 20:20:58 -0700

    Add don't-nest comment to selectFiguresByView.

--- src/app/store/selectors/figures.selectors.ts
+++ src/app/store/selectors/figures.selectors.ts
@@ -4,5 +4,6 @@ import { FiguresState } from './../../models/figures-state.model';
 
 export const selectFigures = createFeatureSelector<FiguresState>('figures');
 
+// do NOT nest selectFiguresByView in another createSelector fn!
 export const selectFiguresByView = (view: string) =>
   createSelector(selectFigures, (figures: FiguresState) => figures[view]);
