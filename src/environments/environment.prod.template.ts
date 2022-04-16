7ecec972a64a4c5ff05b97f31841c6868488c645
Tze-chiu Lei <tclei2009@gmail.com>
2022-04-16 16:51:49 -0700

    Add env-var placeholders for FormMail

--- src/environments/environment.prod.template.ts
+++ src/environments/environment.prod.template.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
+  formMail: {
+    action: '',
+    recipients: '',
+  },
   gtmId: '',
 };
--- src/environments/environment.template.ts
+++ src/environments/environment.template.ts
@@ -4,6 +4,10 @@
 
 export const environment = {
   production: false,
+  formMail: {
+    action: '',
+    recipients: '',
+  },
   gtmId: '',
 };
 
