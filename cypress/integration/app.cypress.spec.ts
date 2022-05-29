6704b4e66d17a766a5d54e4d52089a64c80e174f
Tze-chiu Lei <tclei2009@gmail.com>
2022-05-29 16:17:59 -0700

    Update App-component E2E-spec

--- cypress/integration/app.cypress.spec.ts
+++ cypress/integration/app.cypress.spec.ts
@@ -1,83 +1,33 @@
 import * as cypress from 'cypress';
 
+import { appConfig } from '@app/app.config';
+
 describe('App component', () => {
-  it('Visits the initial app page', () => {
+  beforeEach(() => {
     cy.visit('/');
-    cy.findByTestId('appHeader').should('be.visible');
   });
 
-  context('Router state', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    it('Shows section-header', () => {
-      cy.findByTestId('routerStateHeader').should('be.visible');
-    });
-    it('Shows url:/ for url', () => {
-      cy.findByTestId('routerStateUrl').should('have.text', '/home');
-    });
-    it('Shows root.outlet:primary for root.outlet', () => {
-      cy.findByTestId('routerStateRootOutlet').should('have.text', 'primary');
-    });
+  it('should display Header', () => {
+    cy.get('header')
+      .should('exist')
+      .within(($header) => {
+        cy.wrap($header).find('app-header').should('be.visible');
+      });
   });
 
-  context('Splash Video state', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    it('Shows section-header', () => {
-      cy.findByTestId('splashVideoStateHeader').should('be.visible');
-    });
-    it('Shows done: false as initial state', () => {
-      cy.findByTestId('splashVideoStateDone').should('have.text', 'false');
-    });
-    it('Shows done: true on Finish button click', () => {
-      cy.findByTestId('btnFinishSplashVideo').click();
-      cy.findByTestId('splashVideoStateDone').should('have.text', 'true');
-    });
-    it('Shows done: false on Reset button click', () => {
-      cy.findByTestId('btnResetSplashVideo').click();
-      cy.findByTestId('splashVideoStateDone').should('have.text', 'false');
-    });
+  it('should redirect initial-visit ("/") to Home-view', () => {
+    cy.location('pathname').should('match', /^\/home/);
+    cy.findByText('Home', { selector: '.nav-link' }).should(
+      'have.class',
+      'active',
+    );
   });
 
-  context('Figures state', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    it('Shows section-header', () => {
-      cy.findByTestId('figuresStateHeader').should('be.visible');
-    });
-    it('Shows imageFilenames', () => {
-      cy.get('[data-testclass="imageFilename"]')
-        .should('exist')
-        .and('have.length.at.least', 1);
-    });
-  });
-
-  context('FormMail', () => {
-    beforeEach(() => {
-      cy.intercept('POST', 'https://tze1.com/cgi-bin/FormMail.pl', {
-        statusCode: 200,
-        body: {},
-      }).as('mockSubmission');
-      cy.visit('/');
-    });
-
-    it('Shows section header', () => {
-      cy.findByTestId('formMailHeader').should('be.visible');
-    });
-    it('Submits form', () => {
-      cy.get('[name=realname]').type('Tze [TEST ONLY]');
-      cy.get('[name=email]').type('tclei2009@gmail.com');
-      cy.get('[name=subject]').type('Test message from E2E automated-test');
-      cy.get('[name=message]').type('testing, testing... [E2E-spec]');
-      cy.get('#formMailForm').submit();
-      cy.wait('@mockSubmission');
-      cy.url().should('eq', 'https://tze1.com/cgi-bin/FormMail.pl');
-    });
+  it('should set current-view classhooks', () => {
+    cy.get('body').should('have.class', 'home');
+    cy.get('.container').should('have.class', 'home');
+    cy.visit('/view-instance');
+    cy.get('body').should('have.class', 'view-instance');
+    cy.get('.container').should('have.class', 'view-instance');
   });
 });
